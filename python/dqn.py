69318a4013fca77d638bf8efb2bf450ca15dd3ef
Timothy Yong <tyong_23@hotmail.com>
2017-12-09 23:18:10 -0500

    change back to delta velocity for sake of assignment

--- python/dqn.py
+++ python/dqn.py
@@ -6,7 +6,7 @@ import random
 import time
 import os
 import signal
-from environment import BasketballAccelerationEnv
+from environment import BasketballAccelerationEnv, BasketballVelocityEnv
 from core import ContinuousSpace, \
                  DiscreteSpace, \
                  JointProcessor
@@ -46,7 +46,7 @@ def main():
   global stopsig
 
   # create the basketball environment
-  env = BasketballAccelerationEnv(fps=60.0, timeInterval=0.1,
+  env = BasketballVelocityEnv(fps=60.0, timeInterval=0.1,
       goal=[0, 5, 0],
       initialLengths=np.array([0, 0, 1, 1, 0, 1, 1]),
       initialAngles=np.array([0, 45, -10, -10, 0, -10, 0]))
@@ -65,9 +65,9 @@ def main():
     modelFn.load_params(args.load_params)
 
   softmax = lambda s: np.exp(s) / np.sum(np.exp(s))
-  allActions = actionSpace.sampleAll()
+  #allActions = actionSpace.sampleAll()
   policyFn = EpsilonGreedyPolicy(epsilon=args.epsilon,
-      getActionsFn=lambda state: allActions,
+      getActionsFn=lambda state: actionSpace.sample(1024),
       distributionFn=lambda qstate: softmax(modelFn(qstate)))
   dataset = ReplayBuffer()
   if args.logfile:
@@ -104,17 +104,19 @@ def main():
     print("Training...")
     D = dataset.sample(1024, gamma=args.gamma)
     QS0 = np.concatenate([D["states"], D["actions"]], axis=1)
-    #nextActions = D["actions"]  # choosing the max action is a pain, leave that
-    #                            # up to the actor in the actor-critic models
-    Q1 = []
-    for i in range(D["nextStates"].shape[0]):
-      dist = modelFn(np.concatenate([repmat(D["nextStates"][i, :],
-        allActions.shape[0], 1), allActions], axis=1))
-      Q1.append(np.max(dist))
-    #Q1 = modelFn(np.concatenate([D["nextStates"], nextActions], axis=1))
+    nextActions = D["actions"]  # choosing the max action is a pain, leave that
+                                # up to the actor in the actor-critic models
+                                # just use the continuing action
+    Q1 = modelFn(np.concatenate([D["nextStates"], nextActions], axis=1))
+    #Q1 = []
+    #for i in range(D["nextStates"].shape[0]):
+    #  dist = modelFn(np.concatenate([repmat(D["nextStates"][i, :],
+    #    allActions.shape[0], 1), allActions], axis=1))
+    #  Q1.append(np.max(dist))
+    #Q1 = np.array([Q1]).T
     R = np.where(np.array([D["terminal"]]).T,
         np.array([D["rewards"]]).T,
-        np.array([D["rewards"]]).T + args.gamma * np.array([Q1]).T)
+        np.array([D["rewards"]]).T + args.gamma * Q1)
     modelFn.fit({ "data": QS0, "label": R })
 
     print("Reward:", reward if (reward >= 0.00001) else 0, "with Error:",
@@ -126,6 +128,8 @@ def main():
     rollout += 1
     if args.eps_decay and rollout % 100 == 0:
       policyFn.epsilon *= 0.95
+      if policyFn.epsilon < min(0.1, args.epsilon):
+        policyFn.epsilon = min(0.1, args.epsilon)
       print("Epsilon is now:", policyFn.epsilon)
 
   if args.logfile:
